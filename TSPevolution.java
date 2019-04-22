6c101f4bf7a61b71b6fe846b082605004d482adf
sulax20 <31455768+sulax20@users.noreply.github.com>
2019-04-22 10:45:35 -0500

    Update TSPevolution.java

--- TSPevolution.java
+++ TSPevolution.java
@@ -4,8 +4,10 @@ public class TSPevolution {
 	
 	//fitness function that should give a score of our current 
 	//distance from a city visited.
-	public static void fitnessFunction() {
-		
+public static void fitnessFunction(cities[]) {
+	double dist = 0;
+	for(int i = 0; i < cities.length-2; i++)
+		dist += distance(cities[i],cities[i+1]);
+	return -distance
 	}
-
 }
